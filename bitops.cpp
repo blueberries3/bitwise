da4b3d94e62504f4d8538697a08381c9ce596aef
Shekinah Mikaela Ruth Demata <mikaelademata@gmail.com>
2026-09-25 14:10:31 -0400

    Update bitops.cpp

--- bitops.cpp
+++ bitops.cpp
@@ -7,8 +7,13 @@ bool getBit(unsigned int x, int n) {
   return false;
 }
 
+unsigned int setBit(unsigned int x, int n) {
+  return x | (1u << n);
+}
+
 int main() {
   std::cout << std::boolalpha;
   std::cout << getBit(5, 0) << std::endl;
+  std::cout << setBit(0, 3) << std::endl;
   return 0;
 }
